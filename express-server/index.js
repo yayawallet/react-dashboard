438d31ebcb1a2afdf3a28ffdff74bfe8236cdfe5
suraffy <suraffy@gmail.com>
2024-03-25 16:13:17 +0300

    Implement profile route

--- express-server/index.js
+++ express-server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const {getProfile} = require('@yayawallet/node-sdk');
 
 const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
   res.json({product: 'Yayawallet Dashboard'})
-})
+});
+
+app.get('/profile', async (req, res) => {
+  const profile = await getProfile('username');
+  console.log(profile);
+  res.send(profile);
+});
 
 app.listen(port, () => {
   console.log('App running on port ' + port);
-})
+});
