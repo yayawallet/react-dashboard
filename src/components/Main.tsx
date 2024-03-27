9a3a34d11af334a9cc19c57b49833885e8f6c555
suraffy <suraffy@gmail.com>
2024-03-27 15:45:01 +0300

    Populate profile informations in Profile Component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,9 @@
+import Profile from "./Profile";
+
 const Main = () => {
   return (
     <div className="p-4">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
-      exercitationem quibusdam adipisci laboriosam recusandae laborum
-      praesentium, optio debitis, doloremque voluptatum asperiores dolor,
-      quaerat id magnam. Magnam, necessitatibus cum. Ratione, rerum.
+      <Profile />
     </div>
   );
 };
