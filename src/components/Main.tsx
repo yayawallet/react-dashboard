be7f087126bb8d4cb8ff6f8f2aeb7f9a088f9f7a
suraffy <suraffy@gmail.com>
2024-03-28 16:13:37 +0300

    Implement TransferList

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,11 +2,12 @@ import Profile from "./Profile";
 import CreateTransaction from "./CreateTransaction";
 import GenerateQRCode from "./GenerateQRCode";
 import TransactionList from "./TransactionList";
+import TransferList from "./TransferList";
 
 const Main = () => {
   return (
     <div className="p-4">
-      <TransactionList />
+      <TransferList />
     </div>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -119,7 +119,18 @@ const Sidebar = () => {
                   className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
                 >
                   <span className="flex-1 ms-3 whitespace-nowrap">
-                    Transaction List
+                    Transfer List
+                  </span>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Transaction List By User
                   </span>
                 </a>
               </li>
