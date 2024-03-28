51366b953fc7fee0bb219b530dcc070bc02a8921
suraffy <suraffy@gmail.com>
2024-03-28 13:28:37 +0300

    Implement Generate QRCode

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,11 @@
 import Profile from "./Profile";
 import CreateTransaction from "./CreateTransaction";
+import GenerateQRCode from "./GenerateQRCode";
 
 const Main = () => {
   return (
     <div className="p-4">
-      <CreateTransaction />
+      <GenerateQRCode />
     </div>
   );
 };
