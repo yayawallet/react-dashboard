a2ab81e72a2ca255a4643b06c929dc6118c3c957
suraffy <suraffy@gmail.com>
2024-03-29 13:49:00 +0300

    Implement External Account Lookup

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,11 +5,12 @@ import TransactionList from "./TransactionList";
 import TransferList from "./TransferList";
 import VerifyTransactionByID from "./VerifyTransactionID";
 import Transfer from "./TransferFee";
+import ExternalAccountLookup from "./ExternalAccountLookup";
 
 const Main = () => {
   return (
     <div className="p-4">
-      <Transfer />
+      <ExternalAccountLookup />
     </div>
   );
 };
