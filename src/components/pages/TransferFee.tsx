a0cfe2281514fcddfa6becf46950ed7afb1f2630
suraffy <suraffy@gmail.com>
2024-04-03 10:01:08 +0300

    Some Retouches

--- src/components/pages/TransferFee.tsx
+++ src/components/pages/TransferFee.tsx
@@ -144,7 +144,7 @@ const TransferFee = () => {
           type="submit"
           className="text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
         >
-          {isLoading ? "Checking . . ." : "Check Fee"}
+          {isLoading ? "Checking..." : "Check Fee"}
         </button>
       </form>
     </div>
--- src/components/pages/VerifyTransactionID.tsx
+++ src/components/pages/VerifyTransactionID.tsx
@@ -187,7 +187,7 @@ const GetTransactionByID = () => {
           type="submit"
           className="text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
         >
-          {isLoading ? "Verifying . . ." : "Verify"}
+          {isLoading ? "Verifying..." : "Verify"}
         </button>
       </form>
     </div>
