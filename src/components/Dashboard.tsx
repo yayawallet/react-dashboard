cf0fbb7efb03bd6ffa735f3ae10df9521a707928
suraffy <suraffy@gmail.com>
2024-04-11 15:37:36 +0300

    Add a script to format with prettier

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Profile from "./pages/Profile";
-import CreateTransaction from "./pages/CreateTransaction";
-import GenerateQRCode from "./pages/GenerateQRCode";
-import TransactionList from "./pages/TransactionList";
-import TransferList from "./pages/TransferList";
-import VerifyTransactionByID from "./pages/VerifyTransactionID";
-import TransferFee from "./pages/TransferFee";
-import ExternalAccountLookup from "./pages/ExternalAccountLookup";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Layout from "./Layout";
+import Profile from './pages/Profile';
+import CreateTransaction from './pages/CreateTransaction';
+import GenerateQRCode from './pages/GenerateQRCode';
+import TransactionList from './pages/TransactionList';
+import TransferList from './pages/TransferList';
+import VerifyTransactionByID from './pages/VerifyTransactionID';
+import TransferFee from './pages/TransferFee';
+import ExternalAccountLookup from './pages/ExternalAccountLookup';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Layout from './Layout';
 
 const Dashboard = () => {
   return (
