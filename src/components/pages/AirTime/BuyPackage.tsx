0998bb2bb0b4b337953228a8b129ff110c7765ef
suraffy <suraffy@gmail.com>
2024-04-22 16:54:58 +0300

    Fetch and render list of packages

--- src/components/pages/AirTime/BuyPackage.tsx
+++ src/components/pages/AirTime/BuyPackage.tsx
@@ -1,53 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const BuyPackage = () => {
+  const [packages, setPackages] = useState([]);
+
   useEffect(() => {
     axios
       .post(`${import.meta.env.VITE_BASE_URL}/airtime/packages`, {
         phone: '+251967',
       })
-      .then((res) => console.log(res.data));
+      .then((res) => setPackages(res.data));
   });
 
   return (
-    <div className="flex gap-6">
-      <div className="flex flex-col gap-y-2">
-        <div className="border-2 rounded-lg bg-violet-600 text-white w-40 p-3 py-5 flex justify-center text-center cursor-pointer">
-          <span>One-Birr Package</span>
+    <div className="">
+      <div className="flex gap-6">
+        <div className="flex flex-col gap-y-3">
+          <div className="border-2 rounded-lg bg-violet-600 text-white w-40 p-3 py-5 flex justify-center text-center cursor-pointer">
+            <span>One-Birr Package</span>
+          </div>
+          <div className="border-2 rounded-lg w-40 p-3 py-5 flex justify-center text-center cursor-pointer">
+            <span>Good Morning</span>
+          </div>
         </div>
-        <div className="border-2 rounded-lg w-40 p-3 py-5 flex justify-center text-center cursor-pointer">
-          <span>Good Morning</span>
-        </div>
-      </div>
 
-      <div className="flex flex-wrap gap-2">
-        <div className="border-2 rounded-lg w-80 p-3 cursor-pointer">
-          <span>Voice One hour Package</span>
-        </div>
-        <div className="border-2 rounded-lg w-80 p-3 cursor-pointer">
-          <span>SMS One hour Package For 86 SMS: 1 Birr</span>
-        </div>
-        <div className="border-2 rounded-lg w-80 p-3 cursor-pointer">
-          <span>Voice One hour Package</span>
-        </div>
-        <div className="border-2 rounded-lg w-80 p-3 cursor-pointer">
-          <span>SMS One hour Package For 86 SMS: 1 Birr</span>
-        </div>
-        <div className="border-2 rounded-lg w-80 p-3 cursor-pointer">
-          <span>SMS One hour Package For 86 SMS: 1 Birr</span>
-        </div>
-        <div className="border-2 rounded-lg w-80 p-3 cursor-pointer">
-          <span>SMS One hour Package For 86 SMS: 1 Birr</span>
-        </div>
-        <div className="border-2 rounded-lg w-80 p-3 cursor-pointer">
-          <span>SMS One hour Package For 86 SMS: 1 Birr</span>
-        </div>
-        <div className="border-2 rounded-lg w-80 p-3 cursor-pointer">
-          <span>SMS One hour Package For 86 SMS: 1 Birr</span>
-        </div>
-        <div className="border-2 rounded-lg w-80 p-3 cursor-pointer">
-          <span>SMS One hour Package For 86 SMS: 1 Birr</span>
+        <div className="flex flex-wrap gap-x-2 gap-y-4">
+          {packages?.map((pkg) => (
+            <div className="border-2 rounded-lg w-80 px-3 py-2 flex flex-col justify-between hover:bg-violet-50 cursor-pointer">
+              <span>{pkg.name}</span> <br />
+              <span className="inline-block pt-2i text-lg text-violet-900 font-bold">
+                {pkg.amount} ETB
+              </span>
+            </div>
+          ))}
         </div>
       </div>
 
