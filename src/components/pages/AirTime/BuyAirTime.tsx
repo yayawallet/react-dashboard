2f7faaf4071c986f5e9ec6d227bbdc237082c4b2
suraffy <suraffy@gmail.com>
2024-04-22 16:04:32 +0300

    Add a next button

--- src/components/pages/AirTime/BuyAirTime.tsx
+++ src/components/pages/AirTime/BuyAirTime.tsx
@@ -3,38 +3,38 @@ const BuyAirTime = () => {
     <div className="border-2 rounded-lg p-5">
       <h2 className="font-semibold mb-2">Select Denomination</h2>
 
-      <div className="flex flex-wrap gap-4 mb-8">
-        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 cursor-pointer">
+      <div className="flex flex-wrap gap-4 mb-8 text-xl">
+        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 hover:bg-violet-50 cursor-pointer">
           <span className="pr-1">5</span>
-          <span className="text-xs text-gray-400">(ETB)</span>
+          <span className="text-sm text-gray-400">(ETB)</span>
         </div>
-        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 cursor-pointer">
+        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 hover:bg-violet-50 cursor-pointer">
           <span className="pr-1">10</span>
-          <span className="text-xs text-gray-400">(ETB)</span>
+          <span className="text-sm text-gray-400">(ETB)</span>
         </div>
-        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 cursor-pointer">
+        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 hover:bg-violet-50 cursor-pointer">
           <span className="pr-1">15</span>
-          <span className="text-xs text-gray-400">(ETB)</span>
+          <span className="text-sm text-gray-400">(ETB)</span>
         </div>
-        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 cursor-pointer">
+        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 hover:bg-violet-50 cursor-pointer">
           <span className="pr-1">25</span>
-          <span className="text-xs text-gray-400">(ETB)</span>
+          <span className="text-sm text-gray-400">(ETB)</span>
         </div>
-        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 cursor-pointer">
+        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 hover:bg-violet-50 cursor-pointer">
           <span className="pr-1">50</span>
-          <span className="text-xs text-gray-400">(ETB)</span>
+          <span className="text-sm text-gray-400">(ETB)</span>
         </div>
-        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 cursor-pointer">
+        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 hover:bg-violet-50 cursor-pointer">
           <span className="pr-1">100</span>
-          <span className="text-xs text-gray-400">(ETB)</span>
+          <span className="text-sm text-gray-400">(ETB)</span>
         </div>
-        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 cursor-pointer">
+        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 hover:bg-violet-50 cursor-pointer">
           <span className="pr-1">500</span>
-          <span className="text-xs text-gray-400">(ETB)</span>
+          <span className="text-sm text-gray-400">(ETB)</span>
         </div>
-        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 cursor-pointer">
+        <div className="flex justify-center items-center border rounded w-40 p-2 py-5 hover:bg-violet-50 cursor-pointer">
           <span className="pr-1">1000</span>
-          <span className="text-xs text-gray-400">(ETB)</span>
+          <span className="text-sm text-gray-400">(ETB)</span>
         </div>
       </div>
 
@@ -53,6 +53,10 @@ const BuyAirTime = () => {
           />
         </div>
       </div>
+
+      <button className="block mx-auto mt-8 text-white gap-x-2 bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg w-full sm:max-w-56 px-5 py-2 text-center">
+        Next
+      </button>
     </div>
   );
 };
--- src/components/pages/AirTime/BuyPackage.tsx
+++ src/components/pages/AirTime/BuyPackage.tsx
@@ -50,6 +50,10 @@ const BuyPackage = () => {
           <span>SMS One hour Package For 86 SMS: 1 Birr</span>
         </div>
       </div>
+
+      <button className="block mx-auto mt-8 text-white gap-x-2 bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg w-full sm:max-w-56 px-5 py-2 text-center">
+        Next
+      </button>
     </div>
   );
 };
