47fcee173b14174a8a9997254a33e3d3251f3c12
suraffy <suraffy@gmail.com>
2024-04-23 14:49:32 +0300

    Implement modal with open/close functionality and action confirmation

--- src/components/pages/AirTime/BuyPackage.tsx
+++ src/components/pages/AirTime/BuyPackage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import LoadingSpinner from './LoadingSpinner';
+import Modal from './Modal';
 
 const packageCategories = [
   'One-Birr Package',
@@ -21,6 +22,8 @@ const BuyPackage = () => {
   const [categories, setCategories] = useState(packageCategories);
   const [selectedCategory, setSelectedCategory] = useState('');
   const [selectedPackage, setSelectedPackage] = useState('');
+  const [selectedPackageAmount, setSelectedPackageAmount] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     axios
@@ -37,8 +40,19 @@ const BuyPackage = () => {
     setCategories(Array.from(catgSet));
   }, [packages]);
 
+  const payForPackage = () => setOpenModal(true);
+
+  const handleConfirm = () => {
+    setOpenModal(false);
+  };
+
   return (
     <div>
+      <Modal
+        openModal={openModal}
+        onConfirm={handleConfirm}
+        amount={selectedPackageAmount}
+      />
       <div className="flex gap-6 border-2 rounded-lg p-5">
         <div className="flex flex-col gap-y-3">
           {categories.map((c) => (
@@ -63,7 +77,10 @@ const BuyPackage = () => {
                   <div
                     key={pkg.code}
                     className={`border border-violet-200 rounded-lg w-[19.5rem] px-3 py-2 flex flex-col justify-between cursor-pointer ${selectedPackage === pkg.code ? 'bg-violet-100 hover:bg-violet-100 ring-4 ring-violet-300' : 'hover:bg-violet-50'}`}
-                    onClick={() => setSelectedPackage(pkg.code)}
+                    onClick={() => {
+                      setSelectedPackage(pkg.code);
+                      setSelectedPackageAmount(pkg.amount);
+                    }}
                   >
                     <span>{pkg.name.replace(/:\s\d+\sBirr$/, '')}</span> <br />
                     <span className="inline-block pt-2i text-lg text-violet-900 font-bold">
@@ -75,6 +92,7 @@ const BuyPackage = () => {
             <button
               className="block mx-auto mt-10 text-white gap-x-2 bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg w-full sm:max-w-56 px-5 py-2 text-center cursor-pointer"
               disabled={!selectedPackage}
+              onClick={payForPackage}
             >
               Next
             </button>
--- src/components/pages/AirTime/Modal.tsx
+++ src/components/pages/AirTime/Modal.tsx
@@ -1,15 +1,15 @@
-const Modal = () => {
+const Modal = ({ amount, openModal, onConfirm }) => {
   return (
     <div
       id="popup-modal"
-      className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full cursor-not-allowed"
+      className={`bg-black/80 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full cursor-not-allowed ${openModal ? 'flex' : 'hidden'}`}
     >
       <div className="relative p-4 w-full max-w-md max-h-full cursor-auto">
         <div className="relative bg-white rounded-lg shadow">
           <button
             type="button"
             className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-            data-modal-hide="popup-modal"
+            onClick={() => onConfirm(false)}
           >
             <svg
               className="w-3 h-3"
@@ -26,8 +26,8 @@ const Modal = () => {
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
               />
             </svg>
-            <span className="sr-only">Close modal</span>
           </button>
+
           <div className="p-4 md:p-5 text-center">
             <svg
               className="mx-auto mb-4 text-gray-400 w-12 h-12"
@@ -45,19 +45,20 @@ const Modal = () => {
               />
             </svg>
             <h3 className="mb-5 text-lg font-normal text-gray-500">
-              Are you sure you want to pay?
+              Are you sure you want to pay {amount} Birr ?
             </h3>
             <button
               data-modal-hide="popup-modal"
               type="button"
               className="text-white bg-violet-600 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+              onClick={() => onConfirm(true)}
             >
               Yes, I'm sure
             </button>
             <button
-              data-modal-hide="popup-modal"
               type="button"
               className="py-2.5 px-5 ms-3 text-sm font-medium text-violet-900 focus:outline-none bg-white rounded-lg border border-violet-200 hover:bg-gray-100 hover:text-violet-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
+              onClick={() => onConfirm(false)}
             >
               No, cancel
             </button>
