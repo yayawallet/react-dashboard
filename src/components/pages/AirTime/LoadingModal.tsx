04bd171a2882953a77009a362434152de17aeeaf
suraffy <suraffy@gmail.com>
2024-04-24 11:14:22 +0300

    Make background face full height

--- src/components/pages/AirTime/LoadingModal.tsx
+++ src/components/pages/AirTime/LoadingModal.tsx
@@ -6,7 +6,7 @@ const ModalResult = ({ loading }: Props) => {
   return (
     <div
       id="popup-modal"
-      className={`bg-black/80 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full cursor-not-allowed ${loading ? 'flex' : 'hidden'}`}
+      className={`bg-black/80 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-full cursor-not-allowed ${loading ? 'flex' : 'hidden'}`}
     >
       <div
         aria-label="Loading..."
