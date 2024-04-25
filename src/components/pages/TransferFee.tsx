b6d6d1b078d0e6c3845a55d9f043c62bc0fdca4f
suraffy <suraffy@gmail.com>
2024-04-25 09:52:11 +0300

    Layout fix: align to start

--- src/components/pages/TransferFee.tsx
+++ src/components/pages/TransferFee.tsx
@@ -85,7 +85,7 @@ const TransferFee = () => {
       )}
 
       {transferFee && (
-        <div className="flex justify-center sm:mr-10 md:mr-20 lg:mr-28 rounded-lg mb-20">
+        <div className="sm:ml-10 md:ml-20 lg:ml-28 mb-20">
           <p className="text-2xl">
             Transfer Fee to {institution}:
             <span className="text-6xl px-2">{transferFee.fee}</span>
