2562d654b2ad20a23af8c55db8fe02eca1299286
suraffy <suraffy@gmail.com>
2024-04-26 12:05:10 +0300

    Build a pagination component

---
+++ src/components/common/Pagination.tsx
@@ -0,0 +1,33 @@
+interface Props {
+  page: number;
+  pageCount: number;
+  isFetching: boolean;
+  onPageChange: (newPage: number) => void;
+}
+
+const Pagination = ({ page, pageCount, isFetching, onPageChange }: Props) => {
+  return (
+    <div className="my-10 flxe text-center text-lg">
+      <button
+        disabled={page === 1}
+        className="inline-block text-gray-900 border border-slate-300 py-1 px-4 hover:bg-violet-50 disabled:opacity-50 rounded-tl rounded-bl"
+        onClick={() => onPageChange(page - 1)}
+      >
+        Prev
+      </button>
+
+      <button className="inline-block border-y border-violet-500 py-1 px-6 bg-violet-500 text-white">
+        {isFetching ? '...' : page}
+      </button>
+      <button
+        disabled={page >= pageCount}
+        className="inline-block text-gray-900 border border-slate-300 py-1 px-4 hover:bg-violet-50 disabled:opacity-50 rounded-tr rounded-br"
+        onClick={() => onPageChange(page + 1)}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
