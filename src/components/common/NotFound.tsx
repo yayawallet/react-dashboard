0a81be64491371cbfba19e946fdfcb3f226c2226
suraffy <suraffy@gmail.com>
2024-05-14 11:18:44 +0300

    Update NotFound styles

--- src/components/common/NotFound.tsx
+++ src/components/common/NotFound.tsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
     <div className="flex flex-col bg-gray-50 text-gray-600 items-center justify-center px-20 py-5">
-      <h1 className="text-2xl font-semibold mb-4">404 - Not Found</h1>
+      <h1 className="text-2xl font-semibold mb-4">404 - Data Not Found</h1>
     </div>
   );
 };
