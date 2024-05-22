076c46e5aba23ef83e28237339e3cd410a472fbf
suraffy <suraffy@gmail.com>
2024-05-22 15:50:32 +0300

    Build InlineNotification component

---
+++ src/components/common/InlineNotification.tsx
@@ -0,0 +1,28 @@
+interface Props {
+  type: 'success' | 'error';
+  info: string;
+}
+
+const InlineNotification = ({ type, info }: Props) => {
+  return (
+    <div>
+      <div
+        className={`flex items-center p-4 mb-10 text-sm rounded-lg ${type === 'success' ? 'bg-blue-50 text-blue-800' : 'bg-red-50 text-red-800'}`}
+        role="alert"
+      >
+        <svg className="flex-shrink-0 inline w-4 h-4 me-3" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <span className="sr-only">Info</span>
+        <div>
+          <span className="font-medium mr-2">{type === 'success' ? 'Success' : 'Error'}</span>
+          {info}
+        </div>
+      </div>
+
+      <div className="flex items-center p-4 mb-10 text-sm " role="alert"></div>
+    </div>
+  );
+};
+
+export default InlineNotification;
