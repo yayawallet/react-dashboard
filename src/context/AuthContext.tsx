03792f56f850386b141a45db327c9a8fabce8bda
suraffy <surafelay@gmail.com>
2024-05-31 13:47:57 +0300

    Change base url to http://localhost:8000

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,16 +1,38 @@
 import { createContext, useContext, useState, ReactNode } from 'react';
 
-const AuthContext = createContext(undefined);
+type User = {
+  name: string;
+  age: number;
+};
+
+type AuthContextModel = {
+  authUser: User | null;
+  isLoggedIn: boolean;
+  login: () => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextModel | null>(null);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [authUser, setAuthUser] = useState(null);
+  const [authUser, setAuthUser] = useState<User | null>(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => {
+    setAuthUser({ name: 'Surafel', age: 25 });
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setAuthUser(null);
+    setIsLoggedIn(false);
+  };
 
   const value = {
     authUser,
-    setAuthUser,
     isLoggedIn,
-    setIsLoggedIn,
+    login,
+    logout,
   };
 
   return <AuthContext.Provider value={value}> {children} </AuthContext.Provider>;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,7 @@
 // import yayawallet from '../../assets/yayawallet-brand.svg';
 import Authentication from './Authentication/Index';
-import { useAuth } from '../context/AuthContext';
 
 const Home = () => {
-  const user = useAuth();
-  console.log(user);
   return (
     // <div className="flex flex-col justify-center items-center gap-5 mt-20">
     // <img src={yayawallet} alt="" className="w-1/3" />
