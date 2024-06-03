b61647187893421f49f7d86d6e58c270c8c7b31d
suraffy <surafelay@gmail.com>
2024-06-03 15:21:03 +0300

    Rename file

--- src/hooks/useMutation.ts
+++ src/hooks/useMutation.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import useAccessToken from './useAccessToken';
 
-const useMutateData = (key: (string | number)[], path: string, body: object) => {
+const useMutation = (key: (string | number)[], path: string, body: object) => {
   const { accessToken } = useAccessToken();
 
   const baseUrl = import.meta.env.VITE_BASE_URL;
@@ -17,4 +17,4 @@ const useMutateData = (key: (string | number)[], path: string, body: object) => 
   return useQuery({ queryKey: key, queryFn: fetchData });
 };
 
-export default useMutateData;
+export default useMutation;
