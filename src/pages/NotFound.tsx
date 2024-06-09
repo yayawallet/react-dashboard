a8d7ad2eacb280562b657c0aa6f5e6fec77948d5
suraffy <surafelay@gmail.com>
2024-06-09 08:24:35 +0300

    Update 404 Page Not Found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,16 +5,14 @@ const NotFound = () => {
     <div className="bg-white">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-6xl  font-bold lg:text-8xl text-violet-600">404</h1>
-          <p className="mb-4 text-2xl  font-semibold text-gray-700 md:text-3xl">
-            Something's missing.
-          </p>
-          <p className="mb-4 md:text-lg font-light text-gray-500">
-            Sorry, we can't find that page.
+          <h1 className="mb-4 text-6xl  font-bold lg:text-8xl text-gray-600">404</h1>
+          <p className="mb-2 text-2xl  font-semibold text-gray-700 md:text-3xl">Page not found</p>
+          <p className="mb-4 md:text-lg font-normal text-gray-500">
+            Oops! The page you are looking for does not exist.
           </p>
           <Link
             to={'/'}
-            className='className="inline-flex text-white bg-violet-600 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4'
+            className="text-sm px-4 py-2 text-center my-4 inline-flex text-white bg-violet-600 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg"
           >
             Back to HomePage
           </Link>
