4cb5eb9d57d28684a5a10d39d3517693d1259254
suraffy <surafelay@gmail.com>
2024-06-13 21:28:57 +0300

    Build a user setting component

--- src/components/UserSettings.tsx
+++ src/components/UserSettings.tsx
@@ -1,28 +1,41 @@
-import { useState } from 'react';
-import { IoLogOutOutline, IoPersonCircleOutline } from 'react-icons/io5';
+import {
+  IoLogOutOutline,
+  IoPersonCircleOutline,
+  IoKeyOutline,
+  IoLockOpenOutline,
+} from 'react-icons/io5';
 
 const UserSettings = () => {
-  const [user_id, setUser_id] = useState(1);
+  const user_role = localStorage.getItem('user_role') || 'user';
+  const username = localStorage.getItem('username') || '-';
+  const user_id = localStorage.getItem('user_id') || '-';
 
   return (
     <div className="absolute top-16 right-8">
-      <ul className="p-2 border">
-        <li className="flex items-center">
-          <span className="text-xl px-2 py-2">
-            <IoPersonCircleOutline />
+      <ul className="pt-6 shadow rounded text-lg text-gray-600">
+        <li className="flex items-center px-6 pr-24 py-1 cursor-pointer hover:text-blue-600 hover:bg-slate-50">
+          <span className="text-xl px-3 py-2">
+            <IoLockOpenOutline />
           </span>
-          <span>user_id: {user_id}</span>
+          <span>{user_role}</span>
         </li>
 
-        <li className="flex items-center">
-          <span className="text-xl px-2 py-2">
-            <IoPersonCircleOutline />
+        <li className="flex items-center px-6 pr-24 py-1 cursor-pointer hover:text-blue-600 hover:bg-slate-50">
+          <span className="text-xl px-3 py-2">
+            <IoKeyOutline />
           </span>
-          <span>username</span>
+          <span>user id: {user_id}</span>
         </li>
 
-        <li className="flex items-center">
-          <span className="text-xl px-2 py-2">
+        <li className="flex items-center px-6 pr-24 py-1 cursor-pointer hover:text-blue-600 hover:bg-slate-50">
+          <span className="text-2xl px-3 py-2">
+            <IoPersonCircleOutline />
+          </span>
+          <span>{username}</span>
+        </li>
+
+        <li className="flex items-center px-6 pr-16 py-1 border-t mt-4 cursor-pointer hover:text-blue-600 hover:bg-slate-50">
+          <span className="text-2xl px-3 py-2">
             <IoLogOutOutline />
           </span>
           <span>Log Out</span>
