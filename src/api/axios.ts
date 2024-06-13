a1b374d83271512c19208975e2d20d331910b2e1
suraffy <surafelay@gmail.com>
2024-06-13 06:06:14 +0300

    Create two instance of axios for public and private reqeusts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,37 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const baseURL = import.meta.env.VITE_BASE_URL;
 
-export default axios.create({ baseURL });
+export default axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const fetchAccessToken = async () => {
+  const storedAccessToken = Cookies.get('access_token');
+  if (!storedAccessToken) return null;
+
+  return storedAccessToken;
+};
+
+export const authAxios = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+authAxios.interceptors.request.use(
+  async (config) => {
+    const accessToken = await fetchAccessToken();
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
