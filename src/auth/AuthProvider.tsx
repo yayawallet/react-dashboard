d3cd80b4ac316ceba19be67ff031b1a6318175c5
suraffy <surafelay@gmail.com>
2024-06-13 10:29:22 +0300

    Extract role from access token

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -1,8 +1,11 @@
 import { createContext, useContext, useState, ReactNode } from 'react';
 import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
 
 interface AuthContextType {
   isAuthenticated: boolean;
+  role: string;
+  username: string;
   login: (accessToken: string, refreshToken: string, user_id: string, username: string) => void;
   logout: () => void;
 }
@@ -13,14 +16,26 @@ export const useAuth = () => useContext(AuthContext);
 
 const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!Cookies.get('accessToken'));
+  const [role, setRole] = useState<string>(localStorage.getItem('role') || '');
+  const [username, setUsername] = useState<string>(localStorage.getItem('username') || '');
 
   const login = (accessToken: string, refreshToken: string, user_id: string, username: string) => {
+    type TokenType = {
+      roles: string[];
+    };
+
+    const decodedToken: TokenType = jwtDecode(accessToken);
+    const role = decodedToken.roles[0] || '';
+
     Cookies.set('access_token', accessToken);
     Cookies.set('refresh_token', refreshToken);
     localStorage.setItem('user_id', user_id);
     localStorage.setItem('username', username);
+    localStorage.setItem('role', role);
 
     setIsAuthenticated(true);
+    setRole(role);
+    setUsername(localStorage.getItem('username') || '');
   };
 
   const logout = () => {
@@ -28,12 +43,15 @@ const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     Cookies.remove('refresh_token');
     localStorage.removeItem('user_Id');
     localStorage.removeItem('username');
+    localStorage.removeItem('role');
 
     setIsAuthenticated(false);
+    setRole('');
+    setUsername('');
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, role, username, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
