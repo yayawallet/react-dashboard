112f267e74df4a8ef795096f8a9684089ddaf9cf
suraffy <surafelay@gmail.com>
2024-06-14 08:53:44 +0300

    Display BreadCrumbs on the Header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import BreadCrumbs from './BreadCrumbs';
 
 const Header = () => {
   return (
     <div className="h-16 shadow-sm">
       <header className="flex justify-between px-4">
-        <div className="self-end">{/* <BreadCrumbs /> */}</div>
+        <div className="self-end">
+          <BreadCrumbs />
+        </div>
 
         <ul className="flex text-lg ml-auto">
           <li className="hover:bg-blue-50 flex items-center">
