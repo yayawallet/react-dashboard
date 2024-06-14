db40271ff9e6cb444673a50c1867d2929d8d173e
suraffy <surafelay@gmail.com>
2024-06-14 23:00:13 +0300

    Style Header component

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,28 +1,24 @@
-// import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import BreadCrumbs from './BreadCrumbs';
 import UserSettings from '../UserSettings';
-import { useState } from 'react';
+import avater from '../../assets/avater.svg';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="h-16 shadow-sm">
-      <header className="flex justify-between px-4">
-        <div className="self-end">
+      <header className="flex justify-between px-4 md:px-8 h-full">
+        <div className="self-end pb-1">
           <BreadCrumbs />
         </div>
 
-        <ul className="flex text-lg ml-auto">
-          <li
-            className="hover:bg-blue-50 flex items-center font-semibold text-blue-800 p-4"
-            onClick={() => setOpen(!open)}
-          >
-            {/* <Link to="/" className="font-semibold text-blue-800 p-4"> */}
-            YaYa Dashboard
-            {/* </Link> */}
-          </li>
-        </ul>
+        <div
+          className="h-full w-full flex items-center justify-end text-lg font-semibold text-violet-800 cursor-pointer"
+          onClick={() => setOpen(!open)}
+        >
+          <img src={avater} alt="" className="h-full p-3" />
+        </div>
 
         <div className={`${open ? '' : 'hidden'}`}>
           <UserSettings />
