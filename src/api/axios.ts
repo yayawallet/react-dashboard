04f021d4b2d9c4e7efa645b4a0151da2901dfae9
suraffy <surafelay@gmail.com>
2024-06-20 14:09:58 +0300

    fix: Handle 401 errors from axios reponse to trigger logout

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,33 +1,19 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import { useAuth } from '../auth/AuthProvider';
 
 const baseURL = import.meta.env.VITE_BASE_URL;
 
 export default axios.create({
   baseURL,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // },
 });
-
-const fetchAccessToken = async () => {
-  const storedAccessToken = Cookies.get('access_token');
-  if (!storedAccessToken) return null;
-
-  return storedAccessToken;
-};
 
 export const authAxios = axios.create({
   baseURL,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // },
 });
 
 authAxios.interceptors.request.use(
-  async (config) => {
-    const accessToken = await fetchAccessToken();
+  (config) => {
+    const accessToken = Cookies.get('access_token');
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
@@ -41,13 +27,14 @@ authAxios.interceptors.response.use(
   (response) => response,
   (error) => {
     console.log('Axios request failed');
-    console.log('Axios request failed');
-    console.log('Axios request failed');
 
-    // if (error.response && error.response.status === 401) {
-    //   localStorage.clear();
-    //   window.location.href = '/login';
-    // }
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('user');
+      Cookies.remove('access_token');
+      Cookies.remove('refresh_token');
+
+      window.location.href = '/login';
+    }
 
     return Promise.reject(error);
   }
