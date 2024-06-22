28e5dd9ec4ae5a2731c2d5c8dc0e2d8de9eec448
suraffy <surafelay@gmail.com>
2024-06-22 11:55:49 +0300

    Update Index pages

--- src/pages/RecurringContract/Index.tsx
+++ src/pages/RecurringContract/Index.tsx
@@ -1,24 +1,7 @@
 import IndexPage from '../../components/IndexPage';
-import { useAuth } from '../../auth/AuthProvider';
 
 const Index = () => {
-  const { user_role } = useAuth();
-
-  const links = [
-    { path: 'create', name: 'Create Contract' },
-    { path: 'list', name: 'List of Contracts' },
-    { path: 'report', name: 'Contract Report' },
-    { path: 'request-payment', name: 'Request Payment' },
-    { path: 'request-payment/report', name: 'Request Payment Report' },
-  ];
-
-  return (
-    <IndexPage
-      links={links
-        .filter((link) => (user_role === 'clerk' ? link.path !== 'create' : true))
-        .filter((link) => (user_role === 'clerk' ? link.path !== 'request-payment' : true))}
-    />
-  );
+  return <IndexPage parentPath={'recurring-contract'} />;
 };
 
 export default Index;
