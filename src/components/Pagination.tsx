10d238977480f689c7c3d73aa5710bb9d9b1a69c
suraffy <surafelay@gmail.com>
2024-06-25 16:00:47 +0300

    Retouch pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,27 +1,27 @@
 interface Props {
   page: number;
   pageCount: number;
-  isPending: boolean;
+  isLoading: boolean;
   onPageChange: (newPage: number) => void;
 }
 
-const Pagination = ({ page, pageCount, isPending, onPageChange }: Props) => {
+const Pagination = ({ page, pageCount, isLoading, onPageChange }: Props) => {
   return (
-    <div className="my-10 flxe text-center text-lg">
+    <div className="my-4 flxe text-center">
       <button
         disabled={page === 1}
-        className="inline-block text-gray-900 border border-slate-300 py-1 px-4 hover:bg-violet-50 disabled:opacity-50 rounded-tl rounded-bl"
+        className="inline-block text-gray-900 border border-slate-300 py-1 px-3 hover:bg-violet-50 disabled:opacity-50 rounded-tl rounded-bl"
         onClick={() => onPageChange(page - 1)}
       >
         Prev
       </button>
 
-      <button className="inline-block border-y border-violet-500 py-1 px-6 bg-violet-500 text-white">
-        {isPending ? '...' : page}
+      <button className="inline-block border-y border-violet-500 py-1 px-5 bg-violet-500 text-white font-semibold">
+        {isLoading ? '...' : page}
       </button>
       <button
         disabled={page >= pageCount}
-        className="inline-block text-gray-900 border border-slate-300 py-1 px-4 hover:bg-violet-50 disabled:opacity-50 rounded-tr rounded-br"
+        className="inline-block text-gray-900 border border-slate-300 py-1 px-3 hover:bg-violet-50 disabled:opacity-50 rounded-tr rounded-br"
         onClick={() => onPageChange(page + 1)}
       >
         Next
