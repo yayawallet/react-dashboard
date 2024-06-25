91e6ecd817a0c73dcafad104c72b4dc46eb8645c
suraffy <surafelay@gmail.com>
2024-06-25 10:00:52 +0300

    refactor page-container spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,8 @@ export default {
     function ({ addUtilities }) {
       const newUtilities = {
         '.page-container': {
-          padding: '1rem 3rem',
           'max-width': '1536px',
-        },
-        '@media (max-width: 1024px)': {
-          '.page-container': {
-            padding: '0 2rem',
-          },
+          padding: '0 1rem',
         },
         '.table-container': {
           'max-width': '1536px',
