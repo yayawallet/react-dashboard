e2c92dc5bad6486c58cd79450df3ba5dae8a9f79
suraffy <surafelay@gmail.com>
2024-06-26 14:59:39 +0300

    Utility function - date formatter

--- src/utils/table_utils.ts
+++ src/utils/table_utils.ts
@@ -1,3 +1,12 @@
 export const capitalize = (name: string) => {
   return name.toLowerCase().replace(/\b\w{1}/g, (match) => match.toUpperCase());
 };
+
+export const dateFormatter = (timestamp: Date) => {
+  const dateObj = new Date(Number(timestamp) * 1000);
+
+  const date = dateObj.toDateString().replace(/^\w+\s/, '');
+  const time = dateObj.toLocaleTimeString();
+
+  return `${date} - ${time}`;
+};
