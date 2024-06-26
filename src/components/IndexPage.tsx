4caac834c4f9b7238f28c027076e20f84d0fd0b7
suraffy <surafelay@gmail.com>
2024-06-26 13:31:15 +0300

    Update IndexPage

--- src/components/IndexPage.tsx
+++ src/components/IndexPage.tsx
@@ -14,18 +14,23 @@ const IndexPage = ({ parentPath }: Props) => {
     <>
       {outlet || (
         <div className="page-container">
-          <ul className="flex flex-wrap justify-center gap-5 mt-10">
-            {menus?.map((menu) => (
-              <li
-                key={menu.path}
-                className="border border-blue-100 text-center p-4 rounded text-blue-800 hover:text-blue-600 hover:bg-slate-50"
-              >
-                <Link to={menu.path} className="p-6">
-                  {menu.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <div className="flex flex-col items-center p-8 border rounded-lg">
+            <h2 className="text-2xl font-semibold">
+              {parentPath.replace('-', ' ').replace(/\b\w{1}/g, (match) => match.toUpperCase())}
+            </h2>
+            <ul className="flex flex-wrap justify-center gap-5 mt-10">
+              {menus?.map((menu) => (
+                <li
+                  key={menu.path}
+                  className="border border-blue-100 text-center p-4 rounded text-blue-600 hover:text-blue-600 hover:bg-slate-50"
+                >
+                  <Link to={menu.path} className="p-6">
+                    {menu.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       )}
     </>
