a2db5dcbd6f26f7931f67477ad8f7abebde424ea
suraffy <surafelay@gmail.com>
2024-07-02 14:37:58 +0300

    Style create transaction form

--- src/pages/Transaction/CreateTransaction.tsx
+++ src/pages/Transaction/CreateTransaction.tsx
@@ -21,7 +21,7 @@ const CreateTransaction = () => {
 
     validationSchema: Yup.object({
       receiver: Yup.string().required('Required').max(12, 'Must be 12 characters'),
-      amount: Yup.number().required('Required').min(1, 'Amount must cannot be less than 1.00'),
+      amount: Yup.number().required('Required').min(1, 'Amount cannot be less than 1.00'),
       cause: Yup.string().required('Required').max(50, 'Must be 50 characters or less'),
     }),
 
@@ -50,7 +50,7 @@ const CreateTransaction = () => {
 
   return (
     <div className="page-container">
-      <h1 className="text-2xl font-semibold p-2 mb-5">Make Transaction</h1>
+      <h1 className="text-2xl font-semibold p-2 mb-10">Create Transaction</h1>
 
       {errorMessage && <InlineNotification type="error" info={errorMessage} />}
 
@@ -59,98 +59,85 @@ const CreateTransaction = () => {
       )}
 
       <form
-        className="max-w-lg ml-10 mt-16 shadow shadow-gray-300 px-10 py-6 rounded-lg"
+        className="max-w-[var(--form-width-small)] border p-8 pt-6 rounded-xl mx-auto mb-20"
         onSubmit={formik.handleSubmit}
       >
-        <div className="relative z-0 w-full mb-1 mt-10 group">
-          <input
-            type="Text"
-            id="receiver"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            autoComplete="off"
-            disabled={isLoading}
-            onChange={(e) => {
-              formik.handleChange(e);
-              setSelectedUser('');
-            }}
-            value={formik.values.receiver}
-          />
-          <label
-            htmlFor="receiver"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Receiver
-          </label>
+        <div className="grid gap-6 mb-68 md:grid-cols-5">
+          <div className="col-span-3">
+            <label htmlFor="receiver" className="block mb-2 text-sm font-medium text-gray-900">
+              Receiver
+            </label>
+            <input
+              type="text"
+              id="receiver"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="receiver"
+              autoComplete="off"
+              disabled={isLoading}
+              onChange={(e) => {
+                formik.handleChange(e);
+                setSelectedUser('');
+              }}
+              value={formik.values.receiver}
+            />
 
-          <span className="text-xs text-red-600">
-            {formik.touched.receiver && formik.errors.receiver}
-          </span>
-        </div>
+            <SearchUserInline
+              query={formik.values.receiver}
+              onSelecteUser={(value) => {
+                setSelectedUser(value);
+                formik.setFieldValue('receiver', value);
+              }}
+              onUserNotFound={(value) => setUserNotFound(value)}
+            />
+          </div>
 
-        <SearchUserInline
-          query={formik.values.receiver}
-          onSelecteUser={(value) => {
-            setSelectedUser(value);
-            formik.setFieldValue('receiver', value);
-          }}
-          onUserNotFound={(value) => setUserNotFound(value)}
-        />
-
-        <div className="grid md:grid-cols-2 md:gap-6">
-          <div className="relative z-0 w-full mb-10 group">
+          <div className="col-span-2">
+            <label htmlFor="amount" className="block mb-2 text-sm font-medium text-gray-900">
+              Amount
+            </label>
             <input
               type="number"
               id="amount"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="amount"
               autoComplete="off"
               disabled={isLoading}
               onChange={formik.handleChange}
               value={formik.values.amount}
             />
-            <label
-              htmlFor="amount"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Amount
-            </label>
-
-            <span className="text-xs text-red-600">
+            <span className="text-sm text-red-600">
               {formik.touched.amount && formik.errors.amount}
             </span>
           </div>
+        </div>
 
-          <div className="relative z-0 w-full mb-10 group">
-            <input
-              type="text"
-              id="cause"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              autoComplete="off"
-              disabled={isLoading}
-              onChange={formik.handleChange}
-              value={formik.values.cause}
-            />
-            <label
-              htmlFor="cause"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Reason
-            </label>
-
-            <span className="text-xs text-red-600">
-              {formik.touched.cause && formik.errors.cause}
-            </span>
-          </div>
+        <div>
+          <label htmlFor="cause" className="block mb-2 text-sm font-medium text-gray-900">
+            Reason
+          </label>
+          <input
+            type="number"
+            id="cause"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="reason"
+            autoComplete="off"
+            disabled={isLoading}
+            onChange={formik.handleChange}
+            value={formik.values.cause}
+          />
+          <span className="text-sm text-red-600">
+            {formik.touched.cause && formik.errors.cause}
+          </span>
         </div>
 
         <button
           type="submit"
           disabled={userNotFound || !selectedUser || isLoading}
-          className="text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          className="mt-6 text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm w-full sm:w-[200px] px-5 py-2.5 text-center"
         >
-          {isLoading ? 'Sending...' : 'Send Money'}
+          <span style={{ letterSpacing: '0.3px' }}>
+            {isLoading ? 'Please wait...' : 'Send Money'}
+          </span>
         </button>
       </form>
     </div>
