32d8cf6df809fd789cff2f7b0db2c5d45f91688d
suraffy <surafelay@gmail.com>
2024-07-03 15:30:40 +0300

    Build a reusable Select Element component

---
+++ src/components/SelectElement.tsx
@@ -0,0 +1,32 @@
+interface Props {
+  title?: string;
+  options: string[];
+  onSelect: (value: string) => void;
+}
+
+const SelectElement = ({ title, options, onSelect }: Props) => {
+  return (
+    <div className="relative">
+      <select
+        id={title}
+        className="w-full py-2.5 px-6 b-0 rounded focus:ring-4j ring-gray-200 outline-none transition sidebar-scrollbar"
+        style={{ color: '#444', background: '#eee', appearance: 'none' }}
+        onChange={(e) => onSelect(e.target.value)}
+      >
+        <option label={title} className="text-[16px] font-semibold"></option>
+        {options?.map((list, index) => (
+          <option key={index} value={list} className="text-[16px]">
+            {list}
+          </option>
+        ))}
+      </select>
+
+      <span
+        className="custom-select-arrow absolute top-0 right-0 block h-full w-12 rounded-r"
+        style={{ background: '#e0e0e0', pointerEvents: 'none' }}
+      ></span>
+    </div>
+  );
+};
+
+export default SelectElement;
