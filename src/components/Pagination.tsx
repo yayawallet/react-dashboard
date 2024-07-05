5118f369f71d3a1fafd9ef6b5da811761a345a0f
suraffy <surafelay@gmail.com>
2024-07-05 16:11:32 +0300

    Minor style change

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,28 +1,28 @@
 interface Props {
-  page: number;
+  currentPage: number;
   pageCount: number;
   isLoading: boolean;
   onPageChange: (newPage: number) => void;
 }
 
-const Pagination = ({ page, pageCount, isLoading, onPageChange }: Props) => {
+const Pagination = ({ currentPage, pageCount, isLoading, onPageChange }: Props) => {
   return (
     <div className="my-4 flxe text-[15px] text-center">
       <button
-        disabled={page === 1}
+        disabled={currentPage === 1}
         className="inline-block text-gray-900 border border-slate-300 py-1 px-3 hover:bg-violet-50 disabled:opacity-50 rounded-tl rounded-bl"
-        onClick={() => onPageChange(page - 1)}
+        onClick={() => onPageChange(currentPage - 1)}
       >
         Prev
       </button>
 
       <button className="inline-block border-y border-violet-500 py-1 px-5 bg-violet-500 text-white font-semibold">
-        {isLoading ? '...' : page}
+        {isLoading ? '...' : currentPage}
       </button>
       <button
-        disabled={page >= pageCount}
+        disabled={currentPage >= pageCount}
         className="inline-block text-gray-900 border border-slate-300 py-1 px-3 hover:bg-violet-50 disabled:opacity-50 rounded-tr rounded-br"
-        onClick={() => onPageChange(page + 1)}
+        onClick={() => onPageChange(currentPage + 1)}
       >
         Next
       </button>
