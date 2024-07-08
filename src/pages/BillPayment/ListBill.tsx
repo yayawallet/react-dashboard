ce4447e22d57a01a2e727369edb074090fd6fdda
suraffy <surafelay@gmail.com>
2024-07-08 16:06:06 +0300

    Implement Refresh Data on button click

--- src/pages/BillPayment/ListBill.tsx
+++ src/pages/BillPayment/ListBill.tsx
@@ -8,22 +8,23 @@ import Error from '../../components/ui/Error';
 import EmptyList from '../../components/ui/EmptyList';
 import { capitalize, formatDate } from '../../utils/table_utils';
 import { GoDotFill } from 'react-icons/go';
+import RefreshButton from '../../components/ui/RefreshButton';
 
 const ListBill = () => {
   const [prevList, setPrevList] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
   const [copiedID, setCopiedID] = useState('');
   const [filterByStatus, setFilterByStatus] = useState('');
   const [filteredBillList, setFilteredBillList] = useState<BillListType[]>([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   const {
     error,
     isLoading,
+    mutate,
     data: { data: billList, lastPage: pageCount } = {},
-  } = usePostData('/bill/list', {});
-
-  console.log(billList);
+  } = usePostData(`/bill/list?p=${currentPage}`, {});
 
   const copyTransactionID = (id: string) => {
     navigator.clipboard.writeText(id);
@@ -50,6 +51,12 @@ const ListBill = () => {
     );
   };
 
+  const handleRefresh = async () => {
+    setIsRefreshing(true);
+    await mutate();
+    setIsRefreshing(false);
+  };
+
   return (
     <div className="table-container">
       {error ? (
@@ -58,25 +65,29 @@ const ListBill = () => {
         <Loading />
       ) : (
         <div className="border border-slate-200 rounded-xl">
-          <div className="flex flex-wrap justify-between items-center m-4">
-            <div className="">
+          <div className="flex flex-wrap justify-between items-center m-4 text-[15px]">
+            <div>
               Filter by:
-              <span
-                className={`inline-flex items-center border bg-gray-100 text-gray-500 px-4 pb-1.5 pt-1 rounded mx-2 cursor-pointer ${filterByStatus === 'paid' ? 'bg-violet-600 text-white' : ''}`}
+              <button
+                className={`mx-3 border bg-gray-50 text-gray-600 px-4 py-1 rounded cursor-pointer ${filterByStatus === 'paid' ? 'bg-violet-600 border-violet-600 text-white' : ''}`}
                 onClick={() => handleFilterBill('paid')}
               >
                 Paid
-              </span>
-              <span
-                className={`inline-flex items-center border bg-gray-100 text-gray-500 px-4 pb-1.5 pt-1 rounded mx-2 cursor-pointer ${filterByStatus === 'pending' ? 'bg-violet-600 text-white' : ''}`}
+              </button>
+              <button
+                className={`border bg-gray-50 text-gray-600 px-4 py-1 rounded cursor-pointer ${filterByStatus === 'pending' ? 'bg-violet-600 border-violet-600 text-white' : ''}`}
                 onClick={() => handleFilterBill('pending')}
               >
                 Pending
-              </span>
+              </button>
             </div>
 
             <div className="w-64">
               <SearchBar onSearch={(query) => handleSearchTransaction(query)} />
+            </div>
+
+            <div onClick={handleRefresh}>
+              <RefreshButton />
             </div>
           </div>
 
@@ -84,18 +95,32 @@ const ListBill = () => {
             <EmptyList />
           ) : (
             <>
-              <div className="overflow-auto">
+              <div className="overflow-auto relative">
+                <div className={`${isRefreshing ? '' : 'hidden'}`}>
+                  <div
+                    className="absolute z-10 bg-gray-400 rounded-full p-1.5"
+                    style={{ top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+                  >
+                    <span
+                      className="inline-block border-white h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                      role="status"
+                    ></span>
+                  </div>
+                  <div className="absolute z-20 h-full w-full"></div>
+                </div>
+
                 <table className="w-full">
                   <thead>
                     <tr>
-                      <th className="text-left px-4 py-3 font-medium">ID</th>
-                      <th className="text-left px-4 py-3 font-medium"></th>
-                      <th className="text-left px-4 py-3 font-medium">Client YaYa Account</th>
-                      <th className="text-left px-4 py-3 font-medium">Customer ID</th>
-                      <th className="text-left px-4 py-3 font-medium">Bill ID</th>
-                      <th className="text-left px-4 py-3 font-medium">Amount</th>
-                      <th className="text-left px-4 py-3 font-medium">Status</th>
-                      <th className="text-left px-4 py-3 font-medium">Due Date</th>
+                      <th className="text-left pl-3 py-3 font-medium">ID</th>
+                      <th className="text-left pl-3 py-3 font-medium">Client Account</th>
+                      <th className="text-left pl-3 py-3 font-medium">Cluster</th>
+                      <th className="text-left pl-3 py-3 font-medium">Customer ID</th>
+                      <th className="text-left pl-3 py-3 font-medium">Bill ID</th>
+                      <th className="text-left pl-3 py-3 font-medium">Amount</th>
+                      <th className="text-left pl-3 py-3 font-medium">Status</th>
+                      <th className="text-left pl-3 py-3 font-medium">Due Date</th>
+                      <th className="text-left pl-3 py-3 font-medium">Actions</th>
                     </tr>
                   </thead>
 
@@ -104,7 +129,7 @@ const ListBill = () => {
                       <tr key={t.id} className="hover:bg-slate-100 text-nowrap">
                         <td
                           title={t.id}
-                          className="relative border-b border-slate-200 p-3 cursor-pointer"
+                          className="relative border-b border-slate-200 pl-3 py-3 cursor-pointer"
                           onClick={() => copyTransactionID(t.id)}
                         >
                           {`${t.id.slice(0, 6)}...${t.id.slice(-2)}`}
@@ -114,38 +139,33 @@ const ListBill = () => {
                             ID Copied
                           </span>
                         </td>
-                        <td className="relative border-b border-slate-200 p-3">
-                          <button
-                            type="button"
-                            className="pt-0.5 pb-1 px-3 text text-violet-900 focus:outline-none bg-white rounded border border-violet-200 hover:bg-violet-100 hover:text-violet-700 focus:z-10 focus:ring-4 focus:ring-violet-100"
-                          >
-                            Detail
-                          </button>
-                        </td>
 
-                        <td className="border-b border-slate-200 p-3">
+                        <td className="border-b border-slate-200 pl-3 py-3">
                           {t.customer_yaya_account ? (
-                            capitalize(t.customer_yaya_account?.name)
-                              .split(' ')
-                              .slice(0, 2)
-                              .join(' ')
+                            t.customer_yaya_account?.account
                           ) : (
                             <span className="text-gray-500">unavailable</span>
                           )}
                         </td>
 
-                        <td className="border-b border-slate-200 p-3">
-                          {capitalize(t.customer_id)}
+                        <td className="border-b border-slate-200 pl-3 py-3">
+                          {t.cluster ? (
+                            capitalize(t.cluster)
+                          ) : (
+                            <span className="text-gray-500">unavailable</span>
+                          )}
                         </td>
 
-                        <td className="border-b border-slate-200 p-3">{capitalize(t.bill_id)}</td>
+                        <td className="border-b border-slate-200 pl-3 py-3">{t.customer_id}</td>
 
-                        <td className="border-b border-slate-200 p-3">
+                        <td className="border-b border-slate-200 pl-3 py-3">{t.bill_id}</td>
+
+                        <td className="border-b border-slate-200 pl-3 py-3">
                           {t.amount?.toFixed(2)}{' '}
                           <span className="text-gray-500 text-sm">{t.currency}</span>
                         </td>
 
-                        <td className="border-b border-slate-200 p-3">
+                        <td className="border-b border-slate-200 pl-3 py-3">
                           <span
                             className={`inline-block align-middle pb-0.5 pr-1 text-[16px] text-${t.status == 'PAID' ? 'green' : 'orange'}-500`}
                           >
@@ -154,8 +174,17 @@ const ListBill = () => {
                           {capitalize(t.status)}
                         </td>
 
-                        <td className="border-b border-slate-200 p-3 text-gray-500">
+                        <td className="border-b border-slate-200 pl-3 py-3 text-gray-500">
                           {formatDate(t.due_at).split('-')[0]}
+                        </td>
+
+                        <td className="relative border-b border-slate-200 pl-3 py-3">
+                          <button
+                            type="button"
+                            className="pt-0.5 pb-1 px-3 focus:outline-none text-black bg-yellow-400 rounded hover:bg-yellow-500 focus:z-10 focus:ring-4 focus:ring-yellow-200"
+                          >
+                            Update
+                          </button>
                         </td>
                       </tr>
                     ))}
