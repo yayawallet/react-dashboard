bbd7bc08020738f02e22818f81f1b8ae8f05578e
suraffy <surafelay@gmail.com>
2024-07-12 13:59:58 +0300

    Fix: button text typo

--- src/pages/GenerateQRCode.tsx
+++ src/pages/GenerateQRCode.tsx
@@ -131,7 +131,7 @@ const GenerateQRCode = () => {
             className="text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm w-full sm:w-[200px] px-5 py-2.5 text-center"
           >
             <span style={{ letterSpacing: '0.3px' }}>
-              {isLoading ? 'Please wait...' : 'Send Money'}
+              {isLoading ? 'Please wait...' : 'Generate QR Code'}
             </span>
           </button>
         </form>
--- src/pages/ScheduledPayment/CreateScheduledPayment.tsx
+++ src/pages/ScheduledPayment/CreateScheduledPayment.tsx
@@ -244,7 +244,7 @@ const Create = () => {
               className="text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm w-full sm:w-[200px] px-5 py-2.5 text-center"
             >
               <span className="text-[15px]" style={{ letterSpacing: '0.3px' }}>
-                {isLoading ? 'Please wait...' : 'Create Bill'}
+                {isLoading ? 'Please wait...' : 'Schedule Payment'}
               </span>
             </button>
           </form>
