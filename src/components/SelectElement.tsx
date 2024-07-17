bfb5963db6ed1e3568d39846180bdb82593ded4e
suraffy <surafelay@gmail.com>
2024-07-17 09:21:13 +0300

    Extend tailwind color - modify tailwind config file

--- src/components/SelectElement.tsx
+++ src/components/SelectElement.tsx
@@ -12,7 +12,7 @@ const SelectElement = ({ title, options, onSelect }: Props) => {
     <div className="relative">
       <select
         id={title}
-        className="w-full py-2.5 px-6 pl-8 b-0 rounded focus:ring-4j ring-gray-200 outline-none transition sidebar-scrollbar"
+        className="w-full py-2.5 px-6 pl-8 b-0 rounded focus:ring-1 ring-gray-200 outline-none transition sidebar-scrollbar"
         style={{ color: '#444', background: '#eee', appearance: 'none' }}
         onChange={(e) => onSelect(e.target.value)}
         autoComplete={title}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ export default {
       height: {
         header: 'var(--header-height)',
       },
+      colors: {
+        violet: {
+          50: '#eef0fa',
+          100: '#d3d4f2',
+          200: '#b5b8e9',
+          300: '#969cde',
+          400: '#787fd2',
+          500: '#5054a2', // <-- Midpoint color
+          600: '#4a4d91',
+          700: '#3e427a',
+          800: '#323663',
+          900: '#292c51',
+        },
+      },
     },
     container: {
       padding: '1rem',
