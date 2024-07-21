03995dbc9e2f309335e079f46c5a6c08865c4bae
suraffy <surafelay@gmail.com>
2024-07-21 13:31:29 +0300

    Search User Inline: add optional props to include self and filter by account type

--- src/components/SearchUserInline.tsx
+++ src/components/SearchUserInline.tsx
@@ -6,10 +6,12 @@ import { useGetData } from '../hooks/useSWR';
 
 interface Props {
   query: string;
+  includeSelf?: boolean;
+  accountType?: string;
   onSelecteUser: (user: string) => void;
 }
 
-const SearchUserInline = ({ query, onSelecteUser }: Props) => {
+const SearchUserInline = ({ query, includeSelf, accountType, onSelecteUser }: Props) => {
   const [usersList, setUsersList] = useState<User[]>([]);
   const [selectedUser, setSelectedUser] = useState('');
   const [userNotFound, setUserNotFound] = useState(false);
@@ -18,7 +20,7 @@ const SearchUserInline = ({ query, onSelecteUser }: Props) => {
   const { data: { account: ownAccount } = {} } = useGetData('/user/profile');
 
   useEffect(() => {
-    if (query.length === 12) return; // username is 12 characters long
+    if (query.length >= 12) return; // username is 12 characters long
 
     setUserNotFound(false);
 
@@ -36,9 +38,19 @@ const SearchUserInline = ({ query, onSelecteUser }: Props) => {
         query: query,
       })
       .then((res) => {
-        setUsersList(res.data.slice(0, 5).filter((user: User) => user.account !== ownAccount));
+        let users = res.data;
 
-        if (res.data.length === 0) {
+        if (accountType?.toLocaleUpperCase() === 'BUSINESS') {
+          users = users.filter((user: User) => user.type?.toUpperCase() === 'BUSINESS');
+        }
+
+        if (includeSelf) {
+          setUsersList(users.slice(0, 5));
+        } else {
+          setUsersList(users.slice(0, 5).filter((user: User) => user.account !== ownAccount));
+        }
+
+        if (users?.length === 0) {
           setUserNotFound(true);
         }
       })
@@ -50,14 +62,14 @@ const SearchUserInline = ({ query, onSelecteUser }: Props) => {
 
   return (
     <div
-      className={`relative z-0 w-full mb-6 ${usersList.length > 0 ? 'border' : ''} border-gray-200 rounded-lg`}
+      className={`relative z-0 w-full ${usersList.length > 0 ? 'border' : ''} border-gray-200 rounded-lg`}
     >
       {usersList.length > 0 ? (
         <div className="bg-gray-0 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 outline-none">
           {usersList?.slice(0, 5).map((user) => (
             <div
               key={user.account}
-              className={`flex gap-2 items-center p-2 border-b border-gray-100 hover:bg-gray-100 cursor-pointer wborder rounded ${selectedUser ? 'bg-violet-500 hover:bg-violet-500 rounded text-white' : ''}`}
+              className={`flex gap-2 items-center px-2 py-1 border-b border-gray-100 hover:bg-gray-100 cursor-pointer wborder rounded ${selectedUser ? 'bg-violet-500 hover:bg-violet-500 rounded text-white' : ''}`}
               onClick={() => {
                 onSelecteUser(user.account);
                 setSelectedUser(user.account);
