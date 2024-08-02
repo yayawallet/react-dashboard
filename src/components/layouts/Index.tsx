76eb38cfdab0b6427da49a563dd6d852e49e5e71
suraffy <surafelay@gmail.com>
2024-08-02 10:12:53 +0300

    feat: add auto-logout on inactivity

--- src/components/layouts/Index.tsx
+++ src/components/layouts/Index.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { useInactivityTimeout } from '../../hooks/useInactivityTimeout';
 
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 const Index = () => {
+  useInactivityTimeout();
+
   return (
     <>
       <div className="lg:ml-[300px] sticky top-0 left-0 right-0 z-10 bg-white">
