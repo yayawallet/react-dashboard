d05189a8c6f677e80ce775312d97285a43602b70
suraffy <surafelay@gmail.com>
2024-08-13 14:28:06 +0300

    Update IndexPage UI

--- src/components/IndexPage.tsx
+++ src/components/IndexPage.tsx
@@ -1,6 +1,7 @@
 import { Link, useOutlet } from 'react-router-dom';
 import { sidebarNavs } from '../routing/navigation';
 import yayaBrank from '../assets/yayawallet-brand.svg';
+import { IoChevronForward } from 'react-icons/io5';
 
 interface Props {
   parentPath: string;
@@ -14,28 +15,31 @@ const IndexPage = ({ parentPath }: Props) => {
   return (
     <>
       {outlet || (
-        <div className="page-container mt-20 flex flex-wrap gap-20 items-center justify-center">
-          <div className="flex flex-col items-center py-8 px-10 border rounded-lg">
-            <h2 className="text-2xl font-semibold">
-              {parentPath.replace('-', ' ').replace(/\b\w{1}/g, (match) => match.toUpperCase())}{' '}
-              Menus
-            </h2>
-            <ul className="flex flex-col flex-wrap justify-center gap-5 mt-10">
+        <div className="page-container">
+          <h3 className="text-2xl font-semibold p-2 mb-10">
+            {parentPath.replace('-', ' ').replace(/\b\w{1}/g, (match) => match.toUpperCase())} Menus
+          </h3>
+
+          <div className="flex flex-wrap gap-x-56 gap-y-4 items-center md:px-20">
+            <ul className="flex flex-col flex-wrap justify-center gap-5">
               {menus?.map((menu) => (
-                <li
-                  key={menu.path}
-                  className="border border-blue-100 text-center rounded text-blue-700 hover:bg-slate-50"
-                >
-                  <Link to={menu.path} className="inline-block py-2 px-6 w-full">
-                    {menu.title}
+                <li key={menu.path} className="md:min-w-[280px]">
+                  <Link
+                    to={menu.path}
+                    className="flex gap-x-5 justify-between bg-gray-100 rounded py-3 px-4 text-gray-800 hover:text-gray-900 hover:bg-gray-200"
+                  >
+                    <span>{menu.title}</span>
+                    <span className="text-lg">
+                      <IoChevronForward />
+                    </span>
                   </Link>
                 </li>
               ))}
             </ul>
           </div>
 
-          <div className="">
-            <img src={yayaBrank} width={'150px'} alt="" />
+          <div className="absolute bottom-5 left-[50vw]">
+            <img src={yayaBrank} width={'120px'} alt="" />
           </div>
         </div>
       )}
