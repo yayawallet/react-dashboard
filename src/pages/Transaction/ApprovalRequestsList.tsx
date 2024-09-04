7e4006e395f0d7c315c20afb3069339b079d38dd
suraffy <surafelay@gmail.com>
2024-09-04 15:28:22 +0300

    ApprovalRquests: update table row hover effects

--- src/pages/Transaction/ApprovalRequestsList.tsx
+++ src/pages/Transaction/ApprovalRequestsList.tsx
@@ -200,7 +200,7 @@ const ApprovalRequestsList = () => {
                     {approvalRequestList.map((t: ApprovalRequesType) => (
                       <React.Fragment key={t.uuid}>
                         <tr
-                          className="hover:bg-slate-100 text-nowrap"
+                          className={`text-nowrap ${showDetailID === t.uuid ? '' : 'hover:bg-slate-100'}`}
                           onClick={() => setShowDetailID(showDetailID === t.uuid ? null : t.uuid)}
                         >
                           <td
