49dda7ce7cf9afbaa281168f7ef3fb472975312e
suraffy <surafelay@gmail.com>
2024-09-07 11:00:04 +0300

    RefreshComponent: update refresh component

--- src/components/ui/RefreshComponent.tsx
+++ src/components/ui/RefreshComponent.tsx
@@ -1,3 +1,5 @@
+import { MediumLoading } from './DotLoader';
+
 interface Props {
   isRefreshing: boolean;
 }
@@ -6,18 +8,18 @@ const RefreshComponent = ({ isRefreshing }: Props) => {
   return (
     <div className={`${isRefreshing ? '' : 'hidden'}`}>
       <div
-        className="absolute z-10 bg-white rounded-full p-1.5"
+        className="absolute z-10"
         style={{
-          top: '30vh',
+          top: '64px',
           left: '50%',
           transform: 'translate(-50%)',
-          boxShadow: '0 0 5px #888',
         }}
       >
-        <span
+        <MediumLoading />
+        {/* <span
           className="inline-block border-gray-400 h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
           role="status"
-        ></span>
+        ></span> */}
       </div>
     </div>
   );
