f0311228e3a8dfe2ac5fed992ca56779be1fcaa7
suraffy <surafelay@gmail.com>
2024-09-20 11:14:33 +0300

    UserRegistration: check is_agent - deny access to user registration pages if is_age is false

--- src/pages/RegisterUsers/Index.tsx
+++ src/pages/RegisterUsers/Index.tsx
@@ -1,11 +1,30 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useState } from 'react';
 import RegistrationMethod from './RegistrationMethod';
 import yayaBrand from '../../assets/yayawallet-brand.svg';
+import { useGetData } from '../../hooks/useSWR';
+import BackButton from '../../components/ui/BackButton';
 
 export const RegistrationContext = createContext(null);
 
 const Index = () => {
   const [store, setStore] = useState({});
+  const { data: profile } = useGetData('user/profile');
+
+  if (profile?.is_agent)
+    return (
+      <div className="text-center">
+        <h1 className="text-2xl lg:text-4xl pt-[10vh] pb-4">
+          Sorry, you don't have permission to access this page.
+        </h1>
+        <p className="text-lg pb-10">
+          {profile?.name}
+          <span className="text-gray-600 pl-0.5 pr-2 text-base">@{profile?.account}</span>
+          is <strong>not</strong> an <strong>Agent</strong>.
+        </p>
+
+        <BackButton gotoPath="/" displayText="Back to Home" />
+      </div>
+    );
 
   return (
     // @ts-ignore
