2e6ace07f85c4baf92d04bfc53311c0ae1f2b0f8
suraffy <surafelay@gmail.com>
2024-10-04 10:38:47 +0300

    TransferList: Implement Filter by date

--- src/pages/BankTransfer/TransferList.tsx
+++ src/pages/BankTransfer/TransferList.tsx
@@ -8,11 +8,16 @@ import { capitalize, formatDate } from '../../utils/table_utils';
 import RefreshButton from '../../components/ui/RefreshButton';
 import Pagination from '../../components/Pagination';
 import RefreshComponent from '../../components/ui/RefreshComponent';
+import { IoIosArrowForward, IoIosArrowUp } from 'react-icons/io';
 
 const TransferList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [copiedID, setCopiedID] = useState('');
   const [isRefreshing, setIsRefreshing] = useState(false);
+  const [openCustomFilter, setOpenCustomFilter] = useState(false);
+  const [filterValue, setFilterValue] = useState('');
+  const [filterStartTime, setFilterStartTime] = useState(0);
+  const [filterEndTime, setFilterEndTime] = useState(0);
 
   const copyTransferID = (id: string) => {
     navigator.clipboard.writeText(id);
@@ -27,7 +32,9 @@ const TransferList = () => {
     mutate,
     isValidating,
     data: { data: transferList, lastPage: pageCount, total: totalPayoutMethods, perPage } = {},
-  } = useGetData(`/transfer/list?p=${currentPage}`);
+  } = useGetData(
+    `/transfer/list?p=${currentPage}${filterStartTime !== 0 ? `&start=${filterStartTime}` : ''}${filterEndTime !== 0 ? `&end=${filterEndTime}` : ''}`
+  );
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
@@ -39,23 +46,170 @@ const TransferList = () => {
     setIsRefreshing(false);
   };
 
+  const getCurrentTime = async () => {
+    // const { data: { time: currentTime } = {} } = await axios.get(
+    //   `${import.meta.env.VITE_GET_TIME_URL}`
+    // );
+
+    const currentTime = new Date().getTime();
+
+    return currentTime;
+  };
+
+  const handleFilterByDate = async (value: string) => {
+    setFilterValue(value);
+
+    // Reset custom filter values
+    setFilterStartTime(0);
+    setFilterEndTime(0);
+    if (value !== 'custom') setOpenCustomFilter(false);
+
+    if (!value) return; // If no filter is selected, clear filter
+    if (value === 'custom') return; // If custom filter is selected, the user selecst the date range
+
+    if (transferList && transferList.length === 0) return; // If there is no data, do not filter
+
+    if (value === '1D') {
+      const currentTime = await getCurrentTime();
+      const oneDayAgo = new Date(currentTime / 1000 - 24 * 60 * 60);
+
+      setFilterStartTime(oneDayAgo.getTime());
+      return;
+    }
+
+    if (value === '3D') {
+      const currentTime = await getCurrentTime();
+      const threeDaysAgo = new Date(currentTime / 1000 - 3 * 24 * 60 * 60);
+
+      setFilterStartTime(threeDaysAgo.getTime());
+      return;
+    }
+
+    if (value === '1W') {
+      const currentTime = await getCurrentTime();
+      const oneWeekAgo = new Date(currentTime / 1000 - 7 * 24 * 60 * 60);
+
+      setFilterEndTime(oneWeekAgo.getTime());
+      return;
+    }
+
+    if (value === '1M') {
+      const currentTime = await getCurrentTime();
+      const oneMonthAgo = new Date(currentTime / 1000 - 30 * 24 * 60 * 60);
+
+      setFilterStartTime(oneMonthAgo.getTime());
+      return;
+    }
+  };
+
   return (
     <div className="table-container">
       {error ? (
         <Error />
-      ) : isLoading && currentPage === 1 ? (
-        <Loading />
       ) : (
         <div className="border border-slate-200 rounded-xl">
-          <div className="flex flex-wrap justify-between items-center m-3">
-            <h3 className="py-2 text-lg font-medium">Transfer List</h3>
+          <div className="">
+            <div className="flex flex-wrap justify-between items-center m-3">
+              <h3 className="py-2 text-lg font-medium">Transfer List</h3>
 
-            <div onClick={handleRefresh}>
-              <RefreshButton />
+              <div onClick={handleRefresh}>
+                <RefreshButton />
+              </div>
+            </div>
+
+            <div className="flex flex-wrap items-end mb-8 ml-4 gap-8 mt-6">
+              <div className="flex flex-col items-center gap-2">
+                <div className="text-gray-500 self-start">Filter by date</div>
+                <div className="inline-flex px-4 py-1 gap-1 bg-gray-100 text-gray-800 text-[15px] rounded">
+                  <button
+                    className={`${filterValue === '1D' ? 'bg-yayaBrand-600 text-white' : ''} px-2 py-1 rounded cursor-pointer`}
+                    onClick={() => handleFilterByDate('1D')}
+                    disabled={transferList && transferList.length === 0}
+                  >
+                    1D
+                  </button>
+                  <button
+                    className={`${filterValue === '3D' ? 'bg-yayaBrand-600 text-white' : ''} px-2 py-1 rounded cursor-pointer`}
+                    onClick={() => handleFilterByDate('3D')}
+                    disabled={transferList && transferList.length === 0}
+                  >
+                    3D
+                  </button>
+                  <button
+                    className={`${filterValue === '1W' ? 'bg-yayaBrand-600 text-white' : ''} px-2 py-1 rounded cursor-pointer`}
+                    onClick={() => handleFilterByDate('1W')}
+                    disabled={transferList && transferList.length === 0}
+                  >
+                    1W
+                  </button>
+                  <button
+                    className={`${filterValue === '1M' ? 'bg-yayaBrand-600 text-white' : ''} px-2 py-1 rounded cursor-pointer`}
+                    onClick={() => handleFilterByDate('1M')}
+                    disabled={transferList && transferList.length === 0}
+                  >
+                    1M
+                  </button>
+                  <button
+                    className={`${filterValue === '' ? 'bg-yayaBrand-600 text-white' : ''} px-2 py-1 rounded cursor-pointer`}
+                    onClick={() => handleFilterByDate('')}
+                    disabled={transferList && transferList.length === 0}
+                  >
+                    All
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex items-end gap-3">
+                <button
+                  className={`${filterValue === 'custom' ? 'bg-yayaBrand-600 text-white' : 'text-gray-800 bg-gray-100'} flex items-center gap-1 cursor-pointer px-2.5 pt-1.5 pb-2 rounded`}
+                  onClick={() => {
+                    setOpenCustomFilter(!openCustomFilter);
+                    handleFilterByDate('custom');
+                  }}
+                  disabled={transferList && transferList.length === 0}
+                >
+                  <span>Custom</span>
+                  <span className="mt-1">
+                    {openCustomFilter ? <IoIosArrowUp /> : <IoIosArrowForward />}
+                  </span>
+                </button>
+
+                <div className={`${openCustomFilter ? '' : 'hidden'} flex items-center gap-4`}>
+                  <div className="">
+                    <label htmlFor="start" className="block mb-1 ml-2 text-sm font-medium">
+                      From
+                    </label>
+                    <input
+                      type="datetime-local"
+                      id="start"
+                      max={new Date().toISOString().replace(/:\d{2}\.\d{3}Z$/, '')}
+                      className="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full px-2.5 py-1.5"
+                      onChange={(e) =>
+                        setFilterStartTime(new Date(e.target.value).getTime() / 1000)
+                      }
+                    />
+                  </div>
+
+                  <div className="">
+                    <label htmlFor="end" className="block mb-1 ml-2 text-sm font-medium">
+                      To
+                    </label>
+                    <input
+                      type="datetime-local"
+                      id="end"
+                      max={new Date().toISOString().replace(/:\d{2}\.\d{3}Z$/, '')}
+                      className="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full px-2.5 py-1.5"
+                      onChange={(e) => setFilterEndTime(new Date(e.target.value).getTime() / 1000)}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
-          {transferList.length === 0 ? (
+          {isLoading && currentPage === 1 ? (
+            <Loading />
+          ) : transferList.length === 0 ? (
             <EmptyList />
           ) : (
             <div className="relative">
