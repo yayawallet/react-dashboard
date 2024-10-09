426e419d265e67a99eb0c020b0c3b19f1e0fcafa
suraffy <surafelay@gmail.com>
2024-10-09 11:13:59 +0300

    Filter by date: code refactor

--- src/components/FilterByDateResult.tsx
+++ src/components/FilterByDateResult.tsx
@@ -1,12 +1,12 @@
 import { Cell, Pie, PieChart } from 'recharts';
 import { formatDate } from '../utils/table_utils';
-import { DotLoaderMedium } from './ui/DotLoader';
 
 interface Props {
   filterValue: string;
+  showFilterResult: boolean;
   isLoading: boolean;
-  totalIncoming: number;
-  totalOutgoing: number;
+  incomingSum: number;
+  outgoingSum: number;
   totalTransactions: number;
   customFilterStartTime: number;
   customFilterEndTime: number;
@@ -14,15 +14,16 @@ interface Props {
 
 const FilterByDateResult = ({
   filterValue,
+  showFilterResult,
   isLoading,
-  totalIncoming,
-  totalOutgoing,
+  incomingSum,
+  outgoingSum,
   totalTransactions,
   customFilterStartTime,
   customFilterEndTime,
 }: Props) => {
   return (
-    <div className={`${filterValue ? '' : 'hidden'}  px-2.5 mb-10 rounded-lg md:mx-4`}>
+    <div className={`${showFilterResult ? '' : 'hidden'}  px-2.5 mb-10 rounded-lg md:mx-4`}>
       <h3 className="text-xl font-semibold mb-2">
         Transactions with in{' '}
         {filterValue === '1D' ? (
@@ -34,100 +35,94 @@ const FilterByDateResult = ({
         ) : filterValue === '1M' ? (
           '1 Month'
         ) : filterValue === 'custom' ? (
-          <span className="text-gray-600 font-normal text-lg">{`${customFilterStartTime ? formatDate(new Date(customFilterStartTime * 1000)) : 'custom time'} - ${customFilterEndTime ? formatDate(new Date(customFilterEndTime * 1000)) : 'custom time'}`}</span>
+          <span className="text-gray-600 text-lg">{`${customFilterStartTime ? formatDate(new Date(customFilterStartTime * 1000)) : 'custom time'} - ${customFilterEndTime ? formatDate(new Date(customFilterEndTime * 1000)) : 'current'}`}</span>
         ) : (
           'All Time'
         )}
       </h3>
 
       <div className="flex flex-wrap items-center gap-6">
-        <div className="text-gray-700 border-4 rounded-lg py-2 px-4 inline-block">
-          <div>
+        <div className="text-gray-700 border-t rounded-lg p-5 inline-block bg-slate-50 shadow-md shadow-gray-400">
+          <div className="text-[#008fd6]">
             Total Incoming:{' '}
-            <span className="text-gray-800 text-lg font-medium">
+            <span className="text-lg">
               {isLoading ? (
                 '...'
-              ) : totalIncoming ? (
+              ) : typeof incomingSum === 'number' ? (
                 <span>
-                  {totalIncoming} <span className="text-gray-500 text-base font-normal">ETB</span>
+                  {incomingSum.toFixed(2)} <span className="text-base">ETB</span>
                 </span>
               ) : (
                 '--'
               )}
             </span>
           </div>
-          <div>
+          <div className="text-[#ff6242]">
             Total Outgoing:{' '}
-            <span className="text-gray-800 text-lg font-medium">
+            <span className="text-lg">
               {isLoading ? (
                 '...'
-              ) : totalOutgoing ? (
+              ) : typeof outgoingSum === 'number' ? (
                 <span>
-                  {totalOutgoing} <span className="text-gray-500 text-base font-normal">ETB</span>
+                  {outgoingSum.toFixed(2)} <span className="text-base">ETB</span>
                 </span>
               ) : (
                 '--'
               )}
             </span>
           </div>
-          <div>
+          <div className="text-gray-800">
             Net:{' '}
-            <span className="text-gray-800 text-lg font-medium">
+            <span className="text-lg">
               {isLoading ? (
                 '...'
-              ) : totalIncoming && totalOutgoing ? (
+              ) : typeof incomingSum === 'number' && typeof outgoingSum === 'number' ? (
                 <span>
-                  {totalIncoming - totalOutgoing}{' '}
-                  <span className="text-gray-500 text-base font-normal">ETB</span>
+                  {(incomingSum - outgoingSum).toFixed(2)} <span className="text-base">ETB</span>
                 </span>
               ) : (
                 '--'
               )}
             </span>
           </div>
-          <div>
+          <div className="text-gray-800">
             Total Number of Transactions:{' '}
-            <span className="text-gray-800 text-xl font-semibold">
-              {isLoading ? '...' : totalTransactions}
-            </span>{' '}
+            <span className="text-lg">{isLoading ? '...' : totalTransactions}</span>{' '}
             {totalTransactions > 1 ? 'transactions' : 'transaction'}
           </div>
         </div>
 
-        <div className={`${totalIncoming && totalOutgoing ? '' : 'hidden'} overflow-x-auto`}>
-          {isLoading ? (
-            <div className="w-[80vw] max-w-[460px] h-[160px] flex self-center justify-center items-center">
-              <DotLoaderMedium />
-            </div>
-          ) : (
-            <PieChart width={460} height={160}>
-              <Pie
-                data={[
-                  {
-                    name: 'Total Incoming',
-                    value: isLoading ? 0 : totalIncoming,
-                  },
-                  {
-                    name: 'Total Outgoing',
-                    value: isLoading ? 0 : totalOutgoing,
-                  },
-                ]}
-                dataKey="value"
-                nameKey="name"
-                cx="50%"
-                cy="50%"
-                outerRadius={70}
-                label={({ name, value }) =>
-                  `${name} (${((value / (totalIncoming + totalOutgoing)) * 100).toFixed(0)}%)`
-                }
-                animationDuration={1000}
-                fill="#8884d8"
-              >
-                <Cell fill={'#12abed'} />
-                <Cell fill={'#ff6242'} />
-              </Pie>
-            </PieChart>
-          )}
+        <div
+          className={`py-4 ${typeof incomingSum === 'number' && typeof outgoingSum === 'number' ? '' : 'hidden'} overflow-x-auto`}
+        >
+          <PieChart width={500} height={200}>
+            <Pie
+              data={[
+                {
+                  name: 'Total Incoming',
+                  value: isLoading ? 0 : incomingSum,
+                },
+                {
+                  name: 'Total Outgoing',
+                  value: isLoading ? 0 : outgoingSum,
+                },
+              ]}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              innerRadius={30}
+              outerRadius={70}
+              label={({ name, value }) =>
+                `${name} (${((value / (incomingSum + outgoingSum)) * 100).toFixed(0)}%)`
+              }
+              animationDuration={1000}
+              fill="#8884d8"
+            >
+              <Cell fill={'#008fd6'} />
+              <Cell fill={'#ff6242'} />
+            </Pie>
+          </PieChart>
         </div>
       </div>
     </div>
