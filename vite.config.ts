454748cbfb2ce0ceed77b691a971f005ed2ded9c
suraffy <surafelay@gmail.com>
2024-10-17 11:22:57 +0300

    .env conf: remove dotenv and read env variables from runtime-env.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
-import { config } from 'dotenv';
 import react from '@vitejs/plugin-react';
-
-// Load environment variables from .env file
-config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
