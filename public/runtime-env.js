d6cdd082afbeac1c8714ad856094712e9f55f2bd
suraffy <surafelay@gmail.com>
2024-10-17 15:33:18 +0300

    track runtime-env.js file

---
+++ public/runtime-env.js
@@ -0,0 +1,6 @@
+window.env = {
+  // BASE_URL: "http://196.188.187.66:8000",
+  BASE_URL: 'http://localhost:8000',
+  TRANSACTION_INVOICE_URL: 'https://sandbox.yayawallet.com/en/invoice/transaction',
+  GET_TIME_URL: 'https://yayawallet.com/api/en/time',
+};
