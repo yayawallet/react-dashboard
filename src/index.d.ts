41639f4fecbc24bf6618bf5fb67bdd92f983b550
suraffy <surafelay@gmail.com>
2024-10-17 11:24:15 +0300

    typescript: extend the windows interface to define env object

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,3 +3,13 @@ declare module '*.png';
 declare module '*.jpeg';
 declare module '*.jpg';
 declare module '*.xlsx';
+
+interface Window {
+  env: {
+    BASE_URL: string;
+    TRANSACTION_INVOICE_URL: string;
+    GET_TIME_URL: {
+      time: number;
+    };
+  };
+}
